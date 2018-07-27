ea57b386af50fc61e0cc32078033d5cc685c0217
John Grubbs <johnmlowery@gmail.com>
2018-07-27 17:22:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions and code here
+// Add your functions and code her
+function destructivelyAppendKitten(){
+  kittens.push('Ralph')
+  return kittens
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift('Bob')
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+function appendKitten(){
+  return [...kittens, "Broom"]
+}
